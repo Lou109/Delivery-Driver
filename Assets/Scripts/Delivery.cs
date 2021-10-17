4f38536dd828062af8e01163c2cddc3bf2d2f7da
Louise Collins <louisecollins1971@icloud.com>
2021-10-17 22:35:49 +0100

    24. How To Use Bools

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class Delivery: MonoBehaviour
 {
+    bool hasPackage;
+
+    private void Start()
+    {
+        Debug.Log(hasPackage);
+    }
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch! that really hurt you know");
     }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Package")
         {
             Debug.Log("Picked up package");
+            hasPackage = true;   
         }
-        if (other.tag == "Customer")
+        if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
-        }
-        
+            hasPackage = false;        
+        }     
     }
 }
